d5a8923031f5dc06f955768c9317b5137fb0b3a7
bigveloper <bigveloper@gmail.com>
2021-09-27 02:48:02 +0900

    action creator function increace

--- src/exercise.js
+++ src/exercise.js
@@ -1,15 +1,29 @@
 /**
  * @description redux
  */
+
+// createStore 는 store 를 만들어 주는 함수
+// react project 에서는 단 하나의 store 를 만듦
 import { createStore } from 'redux';
 
+// redux 에서 관리할 state 정의
 const initState = {
     counter: 0,
     text: '',
     list: [],
 };
 
+// action type 정의
+// action type 은 주로 대문자로 작성한다.
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 const CHANGE_TEXT = 'CHANGE_TEXT';
 const ADD_TO_LIST = 'ADD_TO_LIST';
+
+// action creator function 정의
+// action creator function 는 camelCase 로 작성한다.
+function increase() {
+    return {
+        type: INCREASE, // action object 에는 type 값이 필수!!!
+    };
+}
