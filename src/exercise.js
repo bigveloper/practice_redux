bcde9d16b4ae8dfbf1114e896f64655b4d6ca061
bigveloper <bigveloper@gmail.com>
2021-09-27 02:43:38 +0900

    import { createStore } from 'redux';

--- src/exercise.js
+++ src/exercise.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+/**
+ * @description redux
+ */
+import { createStore } from 'redux';
+
+const initState = {
+    counter: 0,
+    text: '',
+    list: [],
+};
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
