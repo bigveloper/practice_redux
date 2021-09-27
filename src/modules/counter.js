ec60f3af6a9b8e7c0b6d3a98d9f81a8c88315824
bigveloper <bigveloper@gmail.com>
2021-09-27 10:55:50 +0900

    counter.js module

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -12,3 +12,34 @@ const DECREASE = 'counter/DECREASE';
 export const setDiff = (diff) => ({ type: SET_DIFF, diff });
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+
+// 초기 상태 선언 //
+
+const initState = {
+    number: 0,
+    diff: 1,
+};
+
+// reducer 선언 //
+// reducer 는 export default 로 내보낸다.
+export default function counter(state = initState, action) {
+    switch (action.type) {
+        case SET_DIFF:
+            return {
+                ...state,
+                diff: action.diff,
+            };
+        case INCREASE:
+            return {
+                ...state,
+                number: state.number + state.diff,
+            };
+        case DECREASE:
+            return {
+                ...state,
+                number: (state.number = state.diff),
+            };
+        default:
+            return state;
+    }
+}
