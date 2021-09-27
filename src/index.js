8d278a86479bac7711da6e239df97723c4b76f13
bigveloper <bigveloper@gmail.com>
2021-09-27 11:41:57 +0900

    go to counter

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import rootReducer from './modules';
 
 const store = createStore(rootReducer); // create store
 // console.log(store.getState());
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 );
 
@@ -20,3 +21,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// react-redux 라이브러리 사용 해야 한다. 설치한다.
+// terminal% npm install react-redux
+
+// 모든게 준비 되었으면 구현 하려 했던  counter 를 구현하러 가자!!
+// src directory 에 components directory 를 생성 후 그 안에 counter.js를 만들어준다.
